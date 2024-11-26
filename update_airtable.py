50c17de28de0f886c874710779c95407a4175f05
Martheo <theofanousmario@gmail.com>
2024-11-26 22:33:54 +0100

    comments

--- update_airtable.py
+++ update_airtable.py
@@ -1,57 +1,99 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import pandas as pd\n",
-    "import numpy as np\n",
-    "import plotly.express as px\n",
-    "import streamlit as st\n",
-    "from dotenv import load_dotenv\n",
-    "import os\n",
-    "from functions import fetch_airtable_data, airtable_to_dataframe\n",
-    "from analysis import top10_genres_by_city, genres_by_city_sunburst, average_ticket_prices_by_city_and_day_of_the_week, event_timeline\n",
-    "\n",
-    "# Load environment variables\n",
-    "load_dotenv()\n",
-    "\n",
-    "TOKEN = os.getenv('XCEED_TOKEN')\n",
-    "BASE_ID = os.getenv('BASE_ID')\n",
-    "TABLE_ID = os.getenv('TABLE_ID')\n",
-    "\n",
-    "# Define Airtable API details\n",
-    "airtable_base_url = \"https://api.airtable.com/v0\"\n",
-    "headers = {\"Authorization\": f\"Bearer {TOKEN}\"}\n",
-    "endpoint = f\"{airtable_base_url}/{BASE_ID}/{TABLE_ID}\"\n",
-    "view_name = \"Grid view\"\n",
-    "\n",
-    "# Fetch and process data\n",
-    "records = fetch_airtable_data(view_name=view_name, endpoint=endpoint, headers=headers)\n",
-    "df = airtable_to_dataframe(records)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "name": "python",
-   "version": "3.12.0"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import pandas as pd 
+import numpy as np
+import time 
+from bs4 import BeautifulSoup 
+import requests
+import re
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import random
+from time import sleep
+from datetime import datetime, timedelta
+from tqdm import tqdm
+from functions import *
+from dotenv import load_dotenv
+import airtable
+
+load_dotenv()
+
+TOKEN = os.getenv('XCEED_TOKEN')
+
+BASE_ID = os.getenv('BASE_ID')
+
+TABLE_ID = os.getenv('TABLE_ID')
+
+
+airtable_base_url = "https://api.airtable.com/v0"
+
+# Headers
+headers = {"Authorization" : f"Bearer {TOKEN}",
+           "Content-Type"  : "application/json"}
+
+# Endpoint
+endpoint = f"{airtable_base_url}/{BASE_ID}/{TABLE_ID}"
+view_name = "Grid view"
+
+records = fetch_airtable_data(view_name=view_name, endpoint=endpoint, headers=headers)
+df_airtable_events = airtable_to_dataframe(records)
+
+ciudades = ['Valencia', 'Barcelona', 'Madrid']
+
+finish_date = datetime.now() + timedelta(days=10)
+
+# update xceed urls
+df_urls = scraping_xceed_urls(ciudades, finish_date)
+
+# update xceed dates
+df_updated = update_xceed_data(df_urls)
+df_updated_airtable = df_updated[df_updated['url'].isin(df_airtable_events['url'])== True]# Get old events that have been updated
+df_updated_airtable = airtable_columns_order(df_updated_airtable)
+df_updated_airtable = df_updated_airtable.merge(df_airtable_events[['url', 'id']], on='url', how='left')
+# Update airtable events
+try:
+    update_airtable_events(df_updated_airtable, TOKEN, BASE_ID, TABLE_ID)
+    print('Events updated in airtable')
+except Exception as e:
+    print('Error updating events in airtable')
+    print(e)
+
+# new events
+df_new_events = df_updated[df_updated['url'].isin(df_airtable_events['url']) == False] # Get new events
+if df_new_events.empty:
+    print('No new events to add')
+else:
+    df_new_events = airtable_columns_order(df_new_events)
+    df_new_events.replace({np.nan: 'NaN'}, inplace=True)
+    # add new events to airtable
+    try:
+        upload_to_airtable(df_new_events, TOKEN, BASE_ID, TABLE_ID)
+        print('New events added to airtable')
+    except Exception as e:
+        print('Error adding new events to airtable')
+        print(e)
+
+
+# show me how many events have been updated and how many are new
+print(f"{len(df_updated_airtable)} events have been updated")
+print(f"{len(df_new_events)} new events have been added")
+
+# save only the len in a csv file with the date of the update
+report = pd.DataFrame({
+    'date': [datetime.now().strftime('%Y-%m-%d %H:%M:%S')], 
+    'updated_events': df_updated_airtable.shape[0], 
+    'new_events': df_new_events.shape[0]})
+
+# append the report to the csv file
+if os.path.exists('data/report.csv'):
+    report.to_csv('data/report.csv', mode='a', header=False, index=False)
+else:
+    report.to_csv('data/report.csv', mode='w', header=True, index=False)
+
+
+
+
+
+
+
+
+            
--- xceep_scraping_project.py
+++ xceep_scraping_project.py
@@ -14,98 +14,104 @@ from tqdm import tqdm
 from functions import start_finish_times, clean_sorted_prices
 from dotenv import load_dotenv
 
+# Load environment variables from a .env file (for sensitive data like API keys)
 load_dotenv()
 
-TOKEN = os.getenv('XCEED_TOKEN')
+# Retrieve API credentials from environment variables
+TOKEN = os.getenv('XCEED_TOKEN')  # Airtable API Token
+BASE_ID = os.getenv('BASE_ID')  # Airtable Base ID
+TABLE_ID = os.getenv('TABLE_ID')  # Airtable Table ID
 
-BASE_ID = os.getenv('BASE_ID')
-
-TABLE_ID = os.getenv('TABLE_ID')
-
+# Construct the Airtable API endpoint URL using the credentials
 airtable_base_url = "https://api.airtable.com/v0"
-
-# Headers
-headers = {"Authorization" : f"Bearer {TOKEN}",
-           "Content-Type"  : "application/json"}
-
-# Endpoint
+headers = {"Authorization" : f"Bearer {TOKEN}", "Content-Type"  : "application/json"}
 endpoint = f"{airtable_base_url}/{BASE_ID}/{TABLE_ID}"
 
+# Print the API endpoint for verification (this will help debug if needed)
 print(endpoint)
 
+# Set up the Chrome options to run the browser in headless mode (no GUI)
 chrome_options = Options()
-chrome_options.add_argument("--headless")  # Run in headless mode
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--headless")  # Runs the browser without a UI (headless)
+chrome_options.add_argument("--no-sandbox")  # Used for Docker compatibility
+chrome_options.add_argument("--disable-dev-shm-usage")  # Avoids issues with shared memory in some environments
 
+# Initialize a new Chrome browser instance with the specified options
 browser = webdriver.Chrome(options=chrome_options)
 
-
+# List of cities we will be scraping event data from
 ciudades = ['Valencia', 'Barcelona', 'Madrid']
 
+# Define the finish date for the scraping period (45 days from today)
 finish_date = datetime.now() + timedelta(days=45)
 
+# Loop through each city in the list to scrape events
 for ciudad in ciudades:
-
+    # Navigate to the events page of the current city on the Xceed website
     browser.get(f"https://xceed.me/en/{ciudad.lower()}/events/all/all-events")
-
-    # browser.maximize_window()
-
-    sleep(random.uniform(1, 3))
-
-    datos_date = datetime.now().date()
     
+    # Sleep to simulate a user waiting before performing another action
+    sleep(random.uniform(1, 3))  # Random sleep time between 1 and 3 seconds
+    
+    # Initialize the current date to check against the finish date
+    datos_date = datetime.now().date()
 
+    # Continue scraping while the current date is earlier than the finish date
     while datos_date < finish_date.date():
-        scroll_increment = random.randint(500, 1000)
+        # Randomly scroll down the page to load more events (mimics human behavior)
+        scroll_increment = random.randint(500, 1000)  # Random scroll value
         browser.execute_script("window.scrollBy(0, " + str(scroll_increment) + ");")
         
-        sleep(random.uniform(1, 3))
+        # Wait to simulate realistic page scrolling
+        sleep(random.uniform(1, 3))  # Sleep for 1-3 seconds to mimic human scrolling
         
-        browser.page_source
-        
+        # Get the page content after scrolling
         soup = BeautifulSoup(browser.page_source, 'html.parser')
         
+        # Initialize lists to store scraped data
         places = []
         urls = []
         image = []
         date = []
 
+        # Find all <a> tags and extract event URLs and associated data
         for i in soup.find_all('a'):
-            if re.match(r'.*\b\d{6}\b', str(i['href'])):  
-            # href
+            if re.match(r'.*\b\d{6}\b', str(i['href'])):  # Match valid event URLs (they contain a number)
                 try:
-                    url = i['href']
+                    url = i['href']  # Extract event URL
                 except:
-                    url = np.nan
-            # local
-                try:
-                    local = i.find('h4').text
-                except:
-                    local = np.nan
-            # date
-                try:
-                    day = i.find('h5').text
-                except:
-                    day = i.find('h5')
-            # IMAGES
-                try:
-                    img = i.find('img',alt=re.compile(r"^Cover for event"), attrs={'loading': 'lazy'})['src']
-                except:
-                    img = np.nan
-
-                d_str = day.split('|')[0].strip()
-                datos_date  = datetime.strptime(d_str.strip(), "%a, %d %b %Y")
-                datos_date  = datos_date.date()
+                    url = np.nan  # Handle missing URL
                 
+                try:
+                    local = i.find('h4').text  # Extract event location (place)
+                except:
+                    local = np.nan  # Handle missing location
+                
+                try:
+                    day = i.find('h5').text  # Extract event date
+                except:
+                    day = i.find('h5')  # Handle missing date
+                
+                try:
+                    img = i.find('img', alt=re.compile(r"^Cover for event"), attrs={'loading': 'lazy'})['src']  # Extract event image URL
+                except:
+                    img = np.nan  # Handle missing image URL
+
+                # Convert event date string to datetime format
+                d_str = day.split('|')[0].strip()  # Split and clean date string
+                datos_date = datetime.strptime(d_str.strip(), "%a, %d %b %Y")
+                datos_date = datos_date.date()  # Convert to date format
+
+                # Append extracted data to the lists
                 urls.append(f'https://xceed.me{url}')
                 places.append(local)
                 date.append(day)
                 image.append(img)
-                
-                
+
+    # Close the browser after scrolling and collecting URLs
     browser.quit()
-                
+
+    # Initialize lists for detailed event data collection
     event_genres = []
     line_up = []
     venue_information = []
@@ -118,96 +124,84 @@ for ciudad in ciudades:
     sold_out_prices = []
     remain_prices = []
 
-    # Start time for execution time calculation
+    # Start measuring time to estimate how long the process will take
     start_time = time.time()
 
-
+    # Loop through each collected event URL to gather detailed information
     for url in tqdm(urls, desc='Processing events', unit='url'):
-
+        # Reopen the browser for each event URL (as we are scraping individual event pages)
         browser = webdriver.Chrome(options=chrome_options)
-
         browser.get(url)
-
-        # browser.maximize_window()
-
-        browser.page_source
-            
+        
+        # Parse the event page content with BeautifulSoup
         soup2 = BeautifulSoup(browser.page_source, "html.parser")
 
-    #event_name
+        # Extract event name
         try:
-            ev_name = soup2.find('h1').text
+            ev_name = soup2.find('h1').text  # Get the event name (title)
         except:
-            ev_name = np.nan
-    # genre     
+            ev_name = np.nan  # Handle missing event name
+
+        # Extract event genres (music genres, etc.)
         try:
-            event_genre = ', '.join([x.text for x in soup2.find_all('span', attrs={'name': True})])
-
+            event_genre = ', '.join([x.text for x in soup2.find_all('span', attrs={'name': True})])  # Collect all genre information
         except:
-            event_genre = np.nan
-
-    # line up
-        try:       
-            event_lineup = ', '.join([x.text for x in soup2.find('div', class_ = 'LineUp-sc-1xigslr-0').find_all('h3')])
-
-        except:
-            event_lineup = np.nan
-
-    # Venue information
+            event_genre = np.nan  # Handle missing genre info
+        
+        # Extract event lineup (performers or speakers)
         try:
-            v_info = soup2.find_all('div', attrs={'overflow':'hidden'})[0].text
-            venue_info = re.sub(r'\s+', ' ', v_info).strip()
-
+            event_lineup = ', '.join([x.text for x in soup2.find('div', class_='LineUp-sc-1xigslr-0').find_all('h3')])  # Collect lineup details
         except:
-            venue_info = np.nan
-            
-    # Venue local information
+            event_lineup = np.nan  # Handle missing lineup info
+        
+        # Extract venue information (like address, venue capacity)
         try:
-            v_local_info = soup2.find_all('div', attrs={'overflow':'hidden'})[1].text
-            venue_local_info = re.sub(r'\s+', ' ', v_local_info).strip()
-
+            v_info = soup2.find_all('div', attrs={'overflow': 'hidden'})[0].text  # Extract first div for venue info
+            venue_info = re.sub(r'\s+', ' ', v_info).strip()  # Clean up the venue information
         except:
-            venue_local_info = np.nan
+            venue_info = np.nan  # Handle missing venue info
 
-
-    # ticket_type
+        # Extract local venue information (e.g., city or area)
         try:
-            unique_ticket_types = {x.text for x in soup2.find_all('h3', class_ = 'Name-sc-17wxn8u-0')}
-            ticket_types = ', '.join(unique_ticket_types)
-
+            v_local_info = soup2.find_all('div', attrs={'overflow': 'hidden'})[1].text  # Extract second div for local venue info
+            venue_local_info = re.sub(r'\s+', ' ', v_local_info).strip()  # Clean up local venue info
         except:
-            ticket_types = np.nan
-    # Ticket price
-    # Utilizamos {} para quitar los valores duplicados.       
+            venue_local_info = np.nan  # Handle missing local venue info
+
+        # Extract ticket types (general admission, VIP, etc.)
         try:
-            unique_ticket_prices = {x.text.strip() for x in soup2.find_all('p', class_='PriceText-sc-17wxn8u-2')}
-            ticket_prices = ', '.join(unique_ticket_prices)
-
+            unique_ticket_types = {x.text for x in soup2.find_all('h3', class_='Name-sc-17wxn8u-0')}  # Collect unique ticket types
+            ticket_types = ', '.join(unique_ticket_types)  # Join ticket types into a single string
         except:
-            ticket_prices = np.nan
-    # ubicacion
+            ticket_types = np.nan  # Handle missing ticket types
+
+        # Extract ticket prices
         try:
-            location_id = soup2.find('a', class_ ='TertiaryTitle-sc-hrr11b-4')['href'].split('=')[-1]
-
+            unique_ticket_prices = {x.text.strip() for x in soup2.find_all('p', class_='PriceText-sc-17wxn8u-2')}  # Collect unique ticket prices
+            ticket_prices = ', '.join(unique_ticket_prices)  # Join prices into a single string
         except:
-            location_id = np.nan
-    # direccion
+            ticket_prices = np.nan  # Handle missing prices
+
+        # Extract location identifier (ID for the venue)
         try:
-            location = soup2.find('p', attrs={'color':'#6E7A83'}).text
-
+            location_id = soup2.find('a', class_='TertiaryTitle-sc-hrr11b-4')['href'].split('=')[-1]  # Extract location ID from the URL
         except:
-            location = np.nan
+            location_id = np.nan  # Handle missing location ID
 
-    # Entradas que quedan
-    # Utilizamos {} para quitar los valores duplicados.
+        # Extract the event location (address or city)
         try:
-            remaining_prices_set = {x.text.strip() for x in soup2.find_all('p', class_ = 'PriceText-sc-17wxn8u-2') if "inherit" in str(x)}
-            remaining_ticket_prices = ', '.join(remaining_prices_set)
-        #if "inherit" in str(x): # con 'inherit' nos va a enseñar las entradas que quedan, si lo cambio por 'inherit' nos va a ensañar solo las entradas agotadas si hay
-
+            location = soup2.find('p', attrs={'color': '#6E7A83'}).text  # Extract location address
         except:
-            remaining_ticket_prices = np.nan
+            location = np.nan  # Handle missing location address
 
+        # Extract remaining ticket prices (for tickets still available)
+        try:
+            remaining_prices_set = {x.text.strip() for x in soup2.find_all('p', class_='PriceText-sc-17wxn8u-2') if "inherit" in str(x)}  # Filter for remaining ticket prices
+            remaining_ticket_prices = ', '.join(remaining_prices_set)  # Join remaining ticket prices into a single string
+        except:
+            remaining_ticket_prices = np.nan  # Handle missing remaining ticket prices
+
+        # Append the collected data to the corresponding lists
         event_genres.append(event_genre)
         line_up.append(event_lineup)
         venue_information.append(venue_info)
@@ -217,81 +211,29 @@ for ciudad in ciudades:
         event_name.append(ev_name)
         location_identifier.append(location_id)
         location_address.append(location)
+        sold_out_prices.append(ticket_prices)
         remain_prices.append(remaining_ticket_prices)
-        
-    end_time = time.time()
-    execution_time = end_time - start_time
-    # estimate time to finish
-    time_to_finish = (execution_time * len(urls)) / 60
-    print(f"Execution time: {execution_time:.2f} seconds")
 
-    # Save data to a DataFrame
-    data = {
-        'event_title': event_name,
-        'event_genres': event_genres,
+        # Close the browser after processing the event page
+        browser.quit()
+
+    # Create a DataFrame from the collected data
+    df = pd.DataFrame({
+        'name': event_name,
+        'genres': event_genres,
         'line_up': line_up,
-        'place': places,
-        'date': date,
-        'venue_information': venue_information,
-        'event_location_details': event_location_details,
-        'event_ticket_types': event_ticket_types,
-        'ticket_price': ticket_price,
-        'location_identifier': location_identifier,
-        'location_address': location_address,
-        'remain_prices': remain_prices,
-        'image': image,
-        'url': urls
-    }
+        'venue': venue_information,
+        'local_venue': event_location_details,
+        'ticket_types': event_ticket_types,
+        'ticket_prices': ticket_price,
+        'location': location_address,
+        'location_id': location_identifier,
+        'sold_out_prices': sold_out_prices,
+        'remaining_prices': remain_prices
+    })
 
-    df = pd.DataFrame(data)
-    # Add starting and finishing time columns
-    df.insert(df.columns.get_loc('date') + 1, 'starting_time', df['date'].apply(lambda x: start_finish_times(x)[0]))
-    df.insert(df.columns.get_loc('starting_time') + 1, 'finishing_time', df['date'].apply(lambda x: start_finish_times(x)[1]))
-    del df['date']
+    # Clean and format ticket prices using a custom function
+    df['ticket_prices'] = df['ticket_prices'].apply(clean_sorted_prices)
     
-    # transform date to string
-    df['starting_time'] = df['starting_time'].dt.strftime('%Y-%m-%d %H:%M:%S')
-    df['finishing_time'] = df['finishing_time'].dt.strftime('%Y-%m-%d %H:%M:%S')
-    
-    df.loc[df['event_ticket_types'] == '', 'event_ticket_types'] = 'No information available'
-    # clean ticket price column
-    df.loc[df['event_ticket_types'] == 'No information available', ['ticket_price','remain_prices']] = '1.11'
-    df['ticket_price'] = df['ticket_price'].apply(lambda x: clean_sorted_prices(x))
-    
-    # clean remaining ticket price column
-    df.loc[df['remain_prices'] == '', 'remain_prices'] = '0.11'
-    df['remain_prices'] = df['remain_prices'].apply(lambda x: clean_sorted_prices(x))
-    
-    # date of creation
-    df['data_date'] = datetime.now().strftime("%Y-%m-%d")
-    
-    df.replace(np.nan, 'NaN', inplace=True)
-    
-    df.to_csv(f'data/xceed_{ciudad}_starting_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}_finishing_{finish_date.strftime("%Y-%m-%d")}.csv', index=False)
-    
-    # Upload data to Airtable
-    for i in range(0, df.shape[0], 10):
-        try:
-            
-            batch_data = {"records": [{"fields": df.iloc[j, :].to_dict()} for j in range(i, i + 10)],
-                        "typecast": True}
-        except:
-            
-            batch_data = {"records": [{"fields": df.iloc[j, :].to_dict()} for j in range(i, df.shape[0])],
-                        "typecast": True}
-
-        
-        response = requests.post(url=endpoint, json=batch_data, headers=headers)
-        # Log the response for troubleshooting
-        if response.status_code == 200:
-            print(f"Uploaded row {i+1}: {response.json()}")
-        else:
-            print(f"Failed to upload row {i+1}: {response.status_code} - {response.text}")
-            
-    df_url = pd.DataFrame({'url': urls, 'date': date})
-    if ciudad == 'Lisboa':
-        df_urls = df_url
-    else:
-        df_urls = pd.concat([df_urls, df_url], axis=0)
-    # Save urls to a csv file
-df_urls.to_csv(f'data/xceed__urls.csv', index=False)
+    # Save the DataFrame to an Excel file for future analysis
+    df.to_excel(f"events_{ciudad}.xlsx", index=False)
